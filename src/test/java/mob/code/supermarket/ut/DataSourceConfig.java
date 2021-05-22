2ad0c5e96f38ceba0034220bcea1a03a085397ed
DengZhiGuo <bobdeng@163.com>
2021-05-22 13:39:41 +0800

    refactor

---
+++ src/test/java/mob/code/supermarket/ut/DataSourceConfig.java
@@ -0,0 +1,28 @@
+package mob.code.supermarket.ut;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DockerImageName dockerImageName = DockerImageName.parse("postgres:11.12");
+        PostgreSQLContainer sqlContainer = new PostgreSQLContainer(dockerImageName);
+        sqlContainer.start();
+        DataSource dataSource = DataSourceBuilder.create()
+                .driverClassName(sqlContainer.getDriverClassName())
+                .username(sqlContainer.getUsername())
+                .password(sqlContainer.getPassword())
+                .url(sqlContainer.getJdbcUrl() + "?characterEncoding=utf-8")
+                .build();
+        return dataSource;
+    }
+
+}
