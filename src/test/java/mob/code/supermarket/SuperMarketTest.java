8db0b7b6b49d0e9935ef42d3628d6be5f5a26970
GeorgeWu <stupidfive@gmail.com>
2021-05-22 11:13:54 +0800

    Add first failing test

---
+++ src/test/java/mob/code/supermarket/SuperMarketTest.java
@@ -0,0 +1,36 @@
+package mob.code.supermarket;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = Application.class)
+@RunWith(SpringRunner.class)
+public class SuperMarketTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void test() throws Exception {
+        System.out.println(mockMvc);
+        mockMvc.perform(post("/api/scan")).andExpect(status().isOk());
+    }
+}
