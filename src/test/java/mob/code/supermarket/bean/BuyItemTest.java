834d79bbec0e40558f467b0ae5c4458816ed8486
DengZhiGuo <bobdeng@163.com>
2021-05-22 17:48:56 +0800

    refactor

--- src/test/java/mob/code/supermarket/bean/BuyItemTest.java
+++ src/test/java/mob/code/supermarket/bean/BuyItemTest.java
@@ -12,7 +12,7 @@ public class BuyItemTest {
     public void 当没有数量缺省为1() throws Exception {
         BuyItem from = BuyItem.from("12345678");
         assertThat(from.getBarcode(), is("12345678"));
-        assertThat(from.getCount(), is(1));
+        assertThat(from.getCount(), is(1.0));
     }
 
 
@@ -20,7 +20,7 @@ public class BuyItemTest {
     public void 当有数量则返回具体数量() throws Exception {
         BuyItem buyItem = BuyItem.from("12345678-2");
         assertThat(buyItem.getBarcode(), is("12345678"));
-        assertThat(buyItem.getCount(), is(2));
+        assertThat(buyItem.getCount(), is(2.0));
     }
 
 
