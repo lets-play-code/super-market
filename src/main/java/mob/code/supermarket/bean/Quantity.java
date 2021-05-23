c31badb411f238cee8717919e474e81e65eee4b8
DengZhiGuo <bobdeng@163.com>
2021-05-23 11:34:13 +0800

    refactor

--- src/main/java/mob/code/supermarket/bean/Quantity.java
+++ src/main/java/mob/code/supermarket/bean/Quantity.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class Quantity {
+    public static final double DOUBLE_MIN = 0.000001;
     private double value;
 
     public Quantity(double value) {
@@ -19,9 +20,14 @@ public class Quantity {
     }
 
     private void ensureIsInteger(double value, String barcode) {
-        if (value - (int) value > 0.000001) {
+        double valueTimes10 = value - (int) value;
+        if (!isZero(valueTimes10)) {
             throw new WrongQuantityException(barcode);
         }
+    }
+
+    private boolean isZero(double value) {
+        return value < DOUBLE_MIN;
     }
 
     public void assertLegal(String barcode) {
@@ -29,7 +35,7 @@ public class Quantity {
     }
 
     public void ensureNotZero(String barcode) {
-        if(value<0.000001){
+        if (isZero(value)) {
             throw new WrongQuantityException(barcode);
         }
     }
