dcd54a15b058d39eae352860261a6214e5af3f14
DengZhiGuo <bobdeng@163.com>
2021-05-23 11:55:36 +0800

    refactor

--- src/test/java/mob/code/supermarket/legacy/BarcodeReaderTest.java
+++ src/test/java/mob/code/supermarket/legacy/BarcodeReaderTest.java
@@ -41,14 +41,25 @@ public class BarcodeReaderTest {
                         "   \n");
         assertThat(barcodeReader.barcodes, is(Arrays.asList("3")));
     }
+
     @Test
     public void input_4() throws Exception {
         BarcodeReader barcodeReader = BarcodeReader.barcodeFactory();
         barcodeReader.getBarcode(
                 "   \n" +
-                        " _|\n" +
-                        " _|\n" +
+                        "|_|\n" +
+                        "  |\n" +
                         "   \n");
         assertThat(barcodeReader.barcodes, is(Arrays.asList("4")));
+    }
+    @Test
+    public void input_5() throws Exception {
+        BarcodeReader barcodeReader = BarcodeReader.barcodeFactory();
+        barcodeReader.getBarcode(
+                " _ \n" +
+                        "|_ \n" +
+                        " _|\n" +
+                        "   \n");
+        assertThat(barcodeReader.barcodes, is(Arrays.asList("5")));
     }
 }
